2660eee408b7909fdaad658b566e01d0d04ade03
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2017-11-03 22:38:11 -0700

    add monitor page test

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use Carbon\Carbon;
+use Spatie\UptimeMonitor\Models\Monitor;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -10,9 +12,31 @@ class HomeTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    function it_can_see_monitors()
+    function it_can_see_monitors_page()
     {
         $this->signIn();
         $this->get('/home')->assertSee('Monitors');
+    }
+
+    /** @test */
+    function it_can_see_a_monitor()
+    {
+        $this->disableExceptionHandling();
+        $url = 'https://www.monasheemountainmultimedia.com';
+        $lookForString = '';
+
+        Monitor::create([
+            'url' => trim($url, '/'),
+            'look_for_string' => $lookForString ?? '',
+            'uptime_check_method' => isset($lookForString) ? 'get' : 'head',
+            'certificate_check_enabled' => true,
+            'uptime_last_check_date' => Carbon::now(),
+            'uptime_check_interval_in_minutes' => 5,
+        ]);
+
+        $this->signIn();
+        $this->get('/home')
+            ->assertSee('https://www.monasheemountainmultimedia.com')
+            ->assertStatus(200);
     }
 }
